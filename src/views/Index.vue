b11042e299f7561778c024266bd35b1275c69be7
unknown <newreport>
2023-07-06 16:16:42 +0800

    testpublic

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -47,7 +47,7 @@ const handleSelect = (key: string, keyPath: string[]) => {
                 </el-row>
             </el-header>
             <el-main>Main111</el-main>
-            <el-footer>Footer222</el-footer>
+            <el-footer>Foottter222</el-footer>
         </el-container>
     </div>
 </template>
